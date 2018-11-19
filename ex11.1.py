b48d538927f6bed6a68c13a3827840f52072722c
Gumsuto <gumicc123@gmail.com>
2018-11-19 11:45:30 +0800

    Update ex11.1.py

--- ex11.1.py
+++ ex11.1.py
@@ -4,8 +4,7 @@ print("You choose Sony or Microsoft?", end=' ')
 Choose = input()
 print("Are you like video games?", end=' ')
 video_games = input("Yes or NO?")
-print("你話打中文得吾得,吾知你得吾得", end=' ')
-China = input("如果得你就打個得,吾得就打No~")
+print("中文測試行", end=' ')
+China = input("是否看得見中文,是否能使用中文輸入")
 
-print(f"生存了 {age} 年 ,我選擇 {Choose} ,我 {video_games} 愛主機遊戲,
-        我 {China} 中文")
+print(f"{age}  ,{Choose} , {video_games} {China} ")
